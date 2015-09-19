212d0d554b20f8eb1ed7d3e8ba71b9156ec5dc2e
Shane Funk <michaelshanefunk@gmail.com>
2015-09-19 10:26:09 -0500

    updating WIP code

--- src/main/funk/shane/hackerrank/java/BigDec.java
+++ src/main/funk/shane/hackerrank/java/BigDec.java
@@ -1,7 +1,6 @@
 package funk.shane.hackerrank.java;
 
-import java.math.BigDecimal;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Shane on 9/13/2015.
@@ -11,12 +10,16 @@ public class BigDec {
         //Input
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String[] s = new String[n+2];
+        String[] s = new String[n];
         for(int i = 0; i < n; i++)
         {
             s[i] = sc.next();
         }
 
+        Set<String> set = new TreeSet<>(Collections.reverseOrder());
+        set.addAll(Arrays.asList(s));
+
+        s = set.toArray(new String[n]);
 
         //Output
         for(int i = 0; i < n; i++)
