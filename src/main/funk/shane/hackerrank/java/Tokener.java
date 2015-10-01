db3200d1d7bf1b0cf6eeaf647a41d5048e3ed301
Shane Funk <michaelshanefunk@gmail.com>
2015-09-30 21:17:02 -0500

    adding completed SQL challenges

---
+++ src/main/funk/shane/hackerrank/java/Tokener.java
@@ -0,0 +1,25 @@
+package funk.shane.hackerrank.java;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * Created by Shane on 9/29/2015.
+ */
+public class Tokener {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().trim();
+        if(str == null || str.length() < 1) {
+            System.out.println("0");
+        }
+        else {
+            String[] words = str.split("[ !,?._'@]+");
+
+            System.out.println(words.length);
+            for (String s : words) {
+                System.out.println(s);
+            }
+        }
+    }
+}
