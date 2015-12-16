c7ac1d3cad0be92bdb5afe505889a24ba79f127f
Shane Funk <michaelshanefunk@gmail.com>
2015-12-15 20:38:04 -0600

    utility file

---
+++ src/main/funk/shane/hackerrank/java/PropTest.java
@@ -0,0 +1,23 @@
+package funk.shane.hackerrank.java;
+
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.TreeSet;
+
+/**
+ * Created by Shane on 12/5/2015.
+ */
+public class PropTest {
+
+    public static void main(String[] args) {
+        Properties p = System.getProperties();
+
+        TreeSet set = new TreeSet(p.keySet());
+        Iterator setIt = set.iterator();
+        while(setIt.hasNext()) {
+            final String key = (String)setIt.next();
+            System.out.printf("%s=[%s]%n", key, p.get(key));
+        }
+
+    }
+}
