01724b7b4c49dba8d6c744abfce9e921e412f5d4
Shane Funk <michaelshanefunk@gmail.com>
2016-01-17 11:51:16 -0600

    finished caesar

--- src/main/funk/shane/hackerrank/algorithms/Caesar.java
+++ src/main/funk/shane/hackerrank/algorithms/Caesar.java
@@ -10,16 +10,33 @@ public class Caesar {
         Scanner sc = new Scanner(System.in);
         int N = Integer.parseInt(sc.nextLine());
         char[] S = (sc.nextLine()).toCharArray();
-        int K = Integer.parseInt(sc.nextLine());
-
-        int ch;
+        int K = Integer.parseInt(sc.nextLine()) % 26;
 
         for(int i = 0; i < S.length; i++) {
             if(Character.isLetter(S[i])) {
                 if(Character.isUpperCase(S[i])) {
-                    ch = S[i];
+                    S[i] = encode(S[i], K, 'A', 'Z');
+                }
+                else if(Character.isLowerCase(S[i])) {
+                    S[i] = encode(S[i], K, 'a', 'z');
                 }
             }
         }
+        System.out.println(S);
+    }
+
+    private static char encode(char targetChar, int key, char lowerChar, char upperChar) {
+        char ch;
+        int delta;
+        int offset = 1;  // array offset for calculation
+        if(targetChar + key > upperChar) {
+            delta = (targetChar + key) - (upperChar + offset);
+            ch = (char)(lowerChar + delta);
+        }
+        else {
+            ch = (char) (targetChar + key);
+        }
+
+        return ch;
     }
 }
